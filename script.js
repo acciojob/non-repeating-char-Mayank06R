7dee55b3680c6701341de5af9049c63dcba1d91e
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-05 19:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = {};
+
+
+    for (const char of str) {
+        if (char in charCount) {
+            charCount[char] += 1;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
